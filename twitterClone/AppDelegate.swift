16d09711e0d6afb4347bf87d16d6c66779cbcbd1
hellyeah <dave@hackmatch.com>
2015-10-01 02:44:37 -0700

    refactor app delegate and login view controller to abstract login code to models and twitter API client; add segue to tweet view controller

--- twitterClone/AppDelegate.swift
+++ twitterClone/AppDelegate.swift
@@ -42,36 +42,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     //where we return to when twitter sends u back
+    //assuming this is a result of a twitter oauth request
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        
-        TwitterClient.sharedInstance.fetchAccessTokenWithPath("oauth/access_token", method: "POST", requestToken: BDBOAuth1Credential(queryString: url.query), success: { (accessToken: BDBOAuth1Credential!) -> Void in
-                print("received access token")
-                TwitterClient.sharedInstance.requestSerializer.saveAccessToken(accessToken)
-            
-                TwitterClient.sharedInstance.GET("1.1/account/verify_credentials.json", parameters: nil, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
-                        //print("user: \(response)")
-                        var user = User(dictionary: response as! NSDictionary)
-                        print("user: \(user.name)")
-                    }, failure: { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
-                        print("error getting current user")
-                })
-            
-                TwitterClient.sharedInstance.GET("1.1/statuses/home_timeline.json", parameters: nil, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
-                        //print("home timeline: \(response)")
-                        var tweets = Tweet.tweetsWithArray(response as! [NSDictionary])
-                        for tweet in tweets {
-                            print("text: \(tweet.text). created \(tweet.createdAt)")
-                        }
-                    }, failure: { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
-                        //
-                        print("error getting home timeline")
-                })
-            
-            }) { (error:NSError!) -> Void in
-                print("failed to receive access token")
-        }
-    
-        
+        TwitterClient.sharedInstance.openURL(url)
         return true
     }
 
--- twitterClone/TwitterClient.swift
+++ twitterClone/TwitterClient.swift
@@ -14,6 +14,8 @@ let twitterBaseUrl = NSURL(string: "https://api.twitter.com")
 
 class TwitterClient: BDBOAuth1RequestOperationManager {
     
+    var loginCompletion: ((user: User?, error: NSError?) -> ())?
+
     class var sharedInstance: TwitterClient {
         struct Static {
             static let instance = TwitterClient(baseURL: twitterBaseUrl, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
@@ -22,4 +24,54 @@ class TwitterClient: BDBOAuth1RequestOperationManager {
         return Static.instance
     }
     
+    func loginWithCompletion(completion: (user: User?, error: NSError?) -> ()) {
+        loginCompletion = completion
+
+        //Fetch request token & redirect to authorization page
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope: nil, success: { (requestToken:BDBOAuth1Credential!) -> Void in
+            print("got the request token")
+            var authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+            UIApplication.sharedApplication().openURL(authURL!)
+            }, failure: { (error: NSError!) -> Void in
+                print("Failed to get request token")
+                self.loginCompletion?(user: nil, error: error)
+        })
+
+    }
+
+    func openURL(url: NSURL) {
+
+        fetchAccessTokenWithPath("oauth/access_token", method: "POST", requestToken: BDBOAuth1Credential(queryString: url.query), success: { (accessToken: BDBOAuth1Credential!) -> Void in
+            print("received access token")
+            TwitterClient.sharedInstance.requestSerializer.saveAccessToken(accessToken)
+
+            TwitterClient.sharedInstance.GET("1.1/account/verify_credentials.json", parameters: nil, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+                //print("user: \(response)")
+                var user = User(dictionary: response as! NSDictionary)
+                print("user: \(user.name)")
+                self.loginCompletion?(user: user, error: nil)
+            }, failure: { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                    print("error getting current user")
+                    self.loginCompletion?(user: nil, error: error)
+            })
+
+            TwitterClient.sharedInstance.GET("1.1/statuses/home_timeline.json", parameters: nil, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+                //print("home timeline: \(response)")
+                var tweets = Tweet.tweetsWithArray(response as! [NSDictionary])
+                for tweet in tweets {
+                    print("text: \(tweet.text). created \(tweet.createdAt)")
+                }
+            }, failure: { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                    //
+                    print("error getting home timeline")
+            })
+
+            }) { (error:NSError!) -> Void in
+                print("failed to receive access token")
+                self.loginCompletion?(user: nil, error: error)
+        }
+
+    }
+
 }
