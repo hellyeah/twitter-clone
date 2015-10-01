b59db2b557eeae98973926e8115f0907d60e4f7d
hellyeah <dave@hackmatch.com>
2015-10-01 00:43:48 -0700

    auth with twitter onLogin

--- twitterClone/ViewController.swift
+++ twitterClone/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController {
         TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
         TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope: nil, success: { (requestToken:BDBOAuth1Credential!) -> Void in
                 print("got the request token")
+            var authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+            UIApplication.sharedApplication().openURL(authURL!)
             }, failure: { (error: NSError!) -> Void in
                 print("Failed to get request token")
             })
